8d51f329fec3b53ccc8144259e6c9bb7296c37dc
bliff182 <michael.bither@gmail.com>
2019-10-02 13:52:45 -0400

    beginning javascript displays properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,3 +21,56 @@
     * Computer chooses a new random word
 
      */
+
+
+
+// array of all possible words to guess 
+var allWords = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten"
+];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+
+var startText = document.getElementById("start-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var currentWordText = document.getElementById("currentword-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+var lettersGuessedText = document.getElementById("lettersguessed-text");
+
+// computer picks random word from array to start
+var randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+
+// blank array to start with underscores corresponding to # of letters in randomWord
+var answerArray = [];
+
+// for loop adds underscore to answerArray for each letter in randomWord
+for (var i = 0; i < randomWord.length; i++) {
+    answerArray[i] = "_";
+}
+
+// displays current word as underscores; .join() method replaces comma separator with space
+currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+
+
+// user hits any key to start
+document.onkeyup = function(event) {
+
+    // pressed key is set to lower case and stored as a variable
+    var userGuess = event.key.toLowerCase();
+
+    // variable to keep track of how many letters are left to be guessed. Will be reduced by 1 with each correct letter
+    var remainingLetters = randomWord.length;
+
+}
