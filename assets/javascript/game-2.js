a18ea022c79a339c13a691869bc0ee0176350326
bliff182 <michael.bither@gmail.com>
2019-10-06 12:29:02 -0400

    game-2.js now linked, game finally working

---
+++ assets/javascript/game-2.js
@@ -0,0 +1,109 @@
+var wins = 0;
+var losses = 0;
+
+// variables for html we want to eventually change
+var startText = document.getElementById("start-text");
+var winLoseText = document.getElementById("win-lose-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var currentWordText = document.getElementById("currentword-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+var lettersGuessedText = document.getElementById("lettersguessed-text");
+
+
+// CLICK BUTTON TO START
+function startGame() {
+
+    // array of all possible words to guess 
+    var allWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+
+    // array to store all possible letter choices 
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    var guessesLeft = 8;
+    var lettersGuessed = [];
+
+    // blank array to start with underscores corresponding to # of letters in randomWord
+    var answerArray = [];
+
+    // computer picks random word from allWords array to start
+    var currentWord = allWords[Math.floor(Math.random() * allWords.length)];
+
+    // variable to keep track of how many letters are left to be guessed. Will be reduced by 1 with each correct letter
+    var remainingLetters = currentWord.length;
+
+
+    // for loop adds underscore to answerArray for each letter in randomWord
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] == " ") {
+            answerArray[i] = " ";
+        }
+        else {
+            answerArray[i] = "_";
+        }
+    }
+
+    winLoseText.textContent = "";
+    // displays current word as underscores; .join() method replaces comma separator with space
+    currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+
+    guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
+    lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
+
+    console.log(currentWord);
+
+    // USER STARTS GUESSING
+    document.onkeyup = function (event) {
+
+        // pressed key is set to lower case and stored as a variable
+        var userGuess = event.key.toLowerCase();
+
+        // check if userGuess is in letters array
+        var letterIndex = letters.indexOf(userGuess);
+
+        // variable to check if guessed letter is in current random word
+        var isInWord = currentWord.includes(userGuess);
+
+        // take userGuess out of letter array to prevent repeat guesses
+        letters.splice(letterIndex, 1);
+
+
+        // code to run if user input is a letter
+        if (letterIndex > -1) {
+            // if userGuess is in currentWord
+            if (isInWord) {
+                for (var j = 0; j < currentWord.length; j++) {
+                    if (currentWord[j] == userGuess) {
+                        answerArray[j] = userGuess;
+                        remainingLetters--;
+                        currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+                    }
+                }
+            }
+
+            else {
+                guessesLeft--;
+                lettersGuessed.push(userGuess);
+                guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
+                lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
+            }
+
+
+        }
+
+// USER WINS
+        if (remainingLetters == 0) {
+            wins++;
+            winLoseText.textContent = "You Win! Click the button to play again!";
+            winsText.textContent = "Wins: " + wins;
+        }
+
+// USER LOSES
+        else if (guessesLeft == 0) {
+            losses++;
+            winLoseText.textContent = "You Lose :( Click the button to play again.";
+            lossesText.textContent = "Losses: " + losses;
+
+        }
+    };
+};
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,21 +26,26 @@
 
 // array of all possible words to guess 
 var allWords = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten"
+    "anarky",
+    "bane",
+    "catwoman",
+    "clayface",
+    "deadshot",
+    "deathstroke",
+    "firefly",
+    "hush",
+    "joker",
+    "penguin",
+    "riddler",
+    "scarecrow",
+    "grundy",
+    "twoface",
+    "zsasz"
 ];
 
 // array to store all possible letter choices 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lettersPicked = [];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
@@ -56,13 +61,14 @@ var lettersGuessedText = document.getElementById("lettersguessed-text");
 //user clicks button to start
 function startGame() {
 
+
     // computer picks random word from array to start
     var currentWord = allWords[Math.floor(Math.random() * allWords.length)];
 
     // variable to keep track of how many letters are left to be guessed. Will be reduced by 1 with each correct letter
     var remainingLetters = currentWord.length;
 
-    var guessesLeft = 10;
+    var guessesLeft = 8;
     var lettersGuessed = [];
 
     // blank array to start with underscores corresponding to # of letters in randomWord
@@ -73,15 +79,12 @@ function startGame() {
         answerArray[i] = "_";
     }
 
-    // Starting text changes to "Start Guessing"
-    // startText.textContent = "Start Guessing!"
-
     // displays current word as underscores; .join() method replaces comma separator with space
     currentWordText.textContent = "Current Word: " + answerArray.join(" ");
 
     guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
     lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
-
+    console.log(currentWord);
 
     // user hits any key to start
     document.onkeyup = function (event) {
@@ -89,38 +92,20 @@ function startGame() {
         // pressed key is set to lower case and stored as a variable
         var userGuess = event.key.toLowerCase();
 
+        // checking if userGuess is in letters array
+        var letterIndex = letters.indexOf(userGuess);
+
         // variable to check if guessed letter is in current random word
         var isInWord = currentWord.includes(userGuess);
+        
 
-        // runs this code if userGuess is in the current word
-        if (isInWord) {
-            // loops through word to look for where guess is in the word
-            for (var j = 0; j < currentWord.length; j++) {
-                if (currentWord[j] === userGuess) {
-                    // replaces underscore in answerArray with guessed letter
-                    answerArray[j] = userGuess;
-                    remainingLetters--;
-                    // updates html for answerArray
-                    currentWordText.textContent = "Current Word: " + answerArray.join(" ");
-                }
-            }
-        }
 
-        else {
-            guessesLeft--;
-            // userGuess is added to lettersGuessed array 
-            lettersGuessed.push(userGuess);
-            // updates html for these elements 
-            guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
-            lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
-        }
-
-        // code to run if user guesses all letters in the word 
+        // USER WINS        
         if (remainingLetters === 0) {
-            alert("you win");
             wins++;
+            alert("you win");
             // these values reset 
-            guessesLeft = 10;
+            guessesLeft = 15;
             lettersGuessed = [];
             answerArray = [];
             currentWord = allWords[Math.floor(Math.random() * allWords.length)];
@@ -130,29 +115,68 @@ function startGame() {
             for (var i = 0; i < currentWord.length; i++) {
                 answerArray[i] = "_";
             }
-            currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+            // currentWordText.textContent = "Current Word: " + answerArray.join(" ");
             lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
             guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
 
-        }
+        } else if (letterIndex > -1) {
 
-        else if (guessesLeft === 0) {
-            alert("you lose");
-            losses++;
-            //these values reset
-            guessesLeft = 10;
-            lettersGuessed = [];
-            answerArray = [];
-            currentWord = allWords[Math.floor(Math.random() * allWords.length)];
-            // losses html gets updated
-            lossesText.textContent = "Losses: " + losses;
-            remainingLetters = currentWord.length;
-            for (var i = 0; i < currentWord.length; i++) {
-                answerArray[i] = "_";
+            // runs this code if userGuess is in the current word
+            if (isInWord) {
+                // loops through word to look for where guess is in the word
+                for (var j = 0; j < currentWord.length; j++) {
+
+                    if (currentWord[j] === userGuess) {
+
+                        // replaces underscore in answerArray with guessed letter
+                        answerArray[j] = userGuess;
+
+                        remainingLetters--;
+
+                        // updates html for answerArray
+                        currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+                    }
+                }
+            } else {
+                guessesLeft--;
+                // userGuess is added to lettersGuessed array 
+                lettersGuessed.push(userGuess);
+                // updates html for these elements 
+                guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
+                lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
             }
-            currentWordText.textContent = "Current Word: " + answerArray.join(" ");
-            lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
-            guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
+
+                if (guessesLeft === 0) {
+                    alert("you lose");
+                    losses++;
+                    //these values reset
+                    guessesLeft = 15;
+                    lettersGuessed = [];
+                    // answerArray = [];
+                    currentWord = allWords[Math.floor(Math.random() * allWords.length)];
+                    // losses html gets updated
+                    lossesText.textContent = "Losses: " + losses;
+                    remainingLetters = currentWord.length;
+                    for (var i = 0; i < currentWord.length; i++) {
+                        answerArray[i] = "_";
+                    }
+                    // currentWordText.textContent = "Current Word: " + answerArray.join(" ");
+                    lettersGuessedText.textContent = "Letters Already Guessed: " + lettersGuessed.join(", ");
+                    guessesLeftText.textContent = "Number of Guesses Remaining: " + guessesLeft;
+                    return;
+                }
+            
+
+            /* for (var k = 0; k < currentWord.word.length; k++){
+                if (answerArray[k] !== userGuess) {
+
+                }
+            } */
+
+            // code to run if user guesses all letters in the word 
+
+
+
         }
 
     }
