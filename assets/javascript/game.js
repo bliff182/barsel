722ba32d1d9e96b2bcad890b12eb211ef3038a6c
bliff182 <michael.bither@gmail.com>
2020-03-09 10:33:44 -0400

    formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,108 +10,163 @@ var currentWordText = document.getElementById("currentword-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var lettersGuessedText = document.getElementById("lettersguessed-text");
 
-
 // CLICK BUTTON TO START
 function startGame() {
+  // array of all possible words to guess
+  var allWords = [
+    "batman",
+    "robin",
+    "joker",
+    "nightwing",
+    "batgirl",
+    "oracle",
+    "gordon",
+    "hawkfire",
+    "alfred",
+    "catwoman",
+    "twoface",
+    "azrael",
+    "riddler",
+    "penguin",
+    "bane",
+    "scarecrow",
+    "clayface",
+    "hush",
+    "cluemaster",
+    "deadshot",
+    "slipknot",
+    "deathstroke",
+    "firefly",
+    "kgbeast",
+    "grundy",
+    "ventriloquist",
+    "zsasz",
+    "enigma",
+    "shiva",
+    "copperhead",
+    "electrocutioner",
+    "phantasm",
+    "manbat",
+    "falcone",
+    "maroni",
+    "anarky",
+    "bullock",
+    "flass",
+    "enchantress",
+    "huntress",
+    "katana"
+  ];
 
-    // array of all possible words to guess 
-    var allWords = ["batman", "robin", "joker", "nightwing", "batgirl", "oracle", "gordon", "hawkfire", "alfred", 
-    "catwoman", "twoface", "azrael", "riddler", "penguin", "bane", "scarecrow", "clayface", "hush", "cluemaster", 
-    "deadshot", "slipknot", "deathstroke", "firefly", "kgbeast", "grundy", "ventriloquist", "zsasz", "enigma", "shiva", 
-    "copperhead", "electrocutioner", "phantasm", "manbat", "falcone", "maroni", "anarky", "bullock", "flass", "enchantress", 
-    "huntress", "katana"];
+  // array to store all possible letter choices
+  var letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
 
-    // array to store all possible letter choices 
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var guessesLeft = 7;
+  var lettersGuessed = [];
 
-    var guessesLeft = 7;
-    var lettersGuessed = [];
+  // blank array to start with underscores corresponding to # of letters in randomWord
+  var answerArray = [];
 
-    // blank array to start with underscores corresponding to # of letters in randomWord
-    var answerArray = [];
+  // computer picks random word from allWords array to start
+  var currentWord = allWords[Math.floor(Math.random() * allWords.length)];
 
-    // computer picks random word from allWords array to start
-    var currentWord = allWords[Math.floor(Math.random() * allWords.length)];
+  // variable to keep track of how many letters are left to be guessed. Will be reduced by 1 with each correct letter
+  var remainingLetters = currentWord.length;
 
-    // variable to keep track of how many letters are left to be guessed. Will be reduced by 1 with each correct letter
-    var remainingLetters = currentWord.length;
+  // for loop adds underscore to answerArray for each letter in randomWord
+  for (var i = 0; i < currentWord.length; i++) {
+    if (currentWord[i] == " ") {
+      answerArray[i] = " ";
+    } else {
+      answerArray[i] = "_";
+    }
+  }
 
+  winLoseText.textContent = "Current Word:";
+  // displays current word as underscores; .join() method replaces comma separator with space
+  currentWordText.textContent = answerArray.join(" ");
 
-    // for loop adds underscore to answerArray for each letter in randomWord
-    for (var i = 0; i < currentWord.length; i++) {
-        if (currentWord[i] == " ") {
-            answerArray[i] = " ";
+  guessesLeftText.textContent = "Guesses Remaining: " + guessesLeft;
+  lettersGuessedText.textContent =
+    "Already Guessed: " + lettersGuessed.join(", ");
+
+  // USER STARTS GUESSING
+  document.onkeyup = function(event) {
+    // pressed key is set to lower case and stored as a variable
+    var userGuess = event.key.toLowerCase();
+
+    // check if userGuess is in letters array
+    var letterIndex = letters.indexOf(userGuess);
+
+    // variable to check if guessed letter is in current random word
+    var isInWord = currentWord.includes(userGuess);
+
+    // take userGuess out of letter array to prevent repeat guesses
+    letters.splice(letterIndex, 1);
+
+    // code to run if user input is a letter
+    if (letterIndex > -1) {
+      // if userGuess is in currentWord
+      if (isInWord) {
+        for (var j = 0; j < currentWord.length; j++) {
+          if (currentWord[j] == userGuess) {
+            answerArray[j] = userGuess;
+            remainingLetters--;
+            currentWordText.textContent = answerArray.join(" ");
+          }
         }
-        else {
-            answerArray[i] = "_";
-        }
+      } else {
+        guessesLeft--;
+        lettersGuessed.push(userGuess);
+        guessesLeftText.textContent = "Guesses Remaining: " + guessesLeft;
+        lettersGuessedText.textContent =
+          "Already Guessed: " + lettersGuessed.join(", ");
+      }
     }
 
-    winLoseText.textContent = "Current Word:";
-    // displays current word as underscores; .join() method replaces comma separator with space
-    currentWordText.textContent = answerArray.join(" ");
+    // USER WINS
+    if (remainingLetters == 0) {
+      wins++;
+      winLoseText.textContent = "You Win! Click the button to play again!";
+      winsText.textContent = "Wins: " + wins;
 
-    guessesLeftText.textContent = "Guesses Remaining: " + guessesLeft;
-    lettersGuessedText.textContent = "Already Guessed: " + lettersGuessed.join(", ");
+      // image of character is revealed -- WHY WON'T THIS RECOGNIZE THE RELATIVE PATH
+      // if (currentWord == "batman") {
+      //     var characterImage = document.getElementById("character-image").src="assets/images/characters/Batman_Vol_3_61_Textless.jpg";
+      // }
+    }
 
-    
-
-    // USER STARTS GUESSING
-    document.onkeyup = function (event) {
-
-        // pressed key is set to lower case and stored as a variable
-        var userGuess = event.key.toLowerCase();
-
-        // check if userGuess is in letters array
-        var letterIndex = letters.indexOf(userGuess);
-
-        // variable to check if guessed letter is in current random word
-        var isInWord = currentWord.includes(userGuess);
-
-        // take userGuess out of letter array to prevent repeat guesses
-        letters.splice(letterIndex, 1);
-
-
-        // code to run if user input is a letter
-        if (letterIndex > -1) {
-            // if userGuess is in currentWord
-            if (isInWord) {
-                for (var j = 0; j < currentWord.length; j++) {
-                    if (currentWord[j] == userGuess) {
-                        answerArray[j] = userGuess;
-                        remainingLetters--;
-                        currentWordText.textContent = answerArray.join(" ");
-                    }
-                }
-            }
-
-            else {
-                guessesLeft--;
-                lettersGuessed.push(userGuess);
-                guessesLeftText.textContent = "Guesses Remaining: " + guessesLeft;
-                lettersGuessedText.textContent = "Already Guessed: " + lettersGuessed.join(", ");
-            }
-
-
-        }
-
-// USER WINS
-        if (remainingLetters == 0) {
-            wins++;
-            winLoseText.textContent = "You Win! Click the button to play again!";
-            winsText.textContent = "Wins: " + wins;
-
-            // image of character is revealed -- WHY WON'T THIS RECOGNIZE THE RELATIVE PATH
-           /*  if (currentWord == "batman") {
-                var characterImage = document.getElementById("character-image").src="../images/characters/Batman_Vol_3_61_Textless";
-            } */
-        }
-
-// USER LOSES
-        else if (guessesLeft == 0) {
-            losses++;
-            winLoseText.textContent = "You Lose :( Click the button to play again";
-            lossesText.textContent = "Losses: " + losses;
-        }
-    };
-};
+    // USER LOSES
+    else if (guessesLeft == 0) {
+      losses++;
+      winLoseText.textContent = "You Lose :( Click the button to play again";
+      lossesText.textContent = "Losses: " + losses;
+    }
+  };
+}
